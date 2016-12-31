d0eae287b3b00838ec63b8f5b568cf5f057a6906
vaironl <vairon@live.com>
2016-12-31 10:33:56 -0500

    Added two main sections of information.

--- index.php
+++ index.php
@@ -1,11 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
 
-This is just a test!
+	<div class="titlecontainer navbar navbar-fixed-top">
+		<h1 class="title container">WhyILoveJapan</h1>
+	</div>
+
+	<div class="container">
+
+		<div class="row">
+			<div class="left-col col-md-4">
+				<h2>Food</h2>
+				<img width="200px" height="200px" src="">
+			</div>
+			<div class="right-col col-md-5 vertical-center">
+				<p>Lore ipsum craptulum rotinium colasdlkajkd aojdnaksd saojd as[od naspkdnkasdiosadpoasd 
+				asdkiasdnaskodokasdn aid naokdn asknd aujn dpia dpiosadiajdpasj da-91-9 iapidpaksdj iajsdpias jpdad
+				 aikpdaspikdapsodpasdjopas</p>
+			 </div>
+		</div>
+		<hr>
+		<div class="row">
+			<div class="left-col col-md-5 vertical-center">
+				<p>Lore ipsum craptulum rotinium colasdlkajkd aojdnaksd saojd as[od naspkdnkasdiosadpoasd 
+				asdkiasdnaskodokasdn aid naokdn asknd aujn dpia dpiosadiajdpasj da-91-9 iapidpaksdj iajsdpias jpdad
+				 aikpdaspikdapsodpasdjopas</p>
+			 </div>
+			 <div class="right-col col-md-4">
+				<h2>Cars</h2>
+				<img width="200px" height="200px" src="">
+			</div>
+		</div>
+
+		<hr>
+		
+	</div>
+
 
 </body>
 </html>
