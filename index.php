7593fee45508a029b0203e3f13999b7ff8de0669
vaironl <vairon@live.com>
2017-01-03 22:12:35 -0500

    Started experimenting with mobile responsiveness.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<title>Home</title>
 	<link rel="stylesheet" type="text/css" href="main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
@@ -22,20 +23,20 @@
 	<div class="container">
 
 		<div class="row">
-			<div class="left-col col-md-4">
+			<div class="left-col col-md-5">
 				<h2>Food</h2>
 				<img class="row-image" src="japan_food.jpg">
 			</div>
-			<div class="right-col col-md-5 vertical-center">
+			<div class="right-col col-md-7 vertical-center">
 				<p class="row-p">One of the traits people tend to like about a culture is the food. I am no exception, because the food in Japan is amazing. Most of us think of sushi when we think of Japanese food. However, there are quite a vast amount of options of food in Japan. My favorite being Mochi!</p>
 			 </div>
 		</div>
 		<hr>
 		<div class="row">
-			<div class="left-col col-md-5 vertical-center">
+			<div class="left-col col-md-7 vertical-center">
 				<p class="row-p">My love for cars started quite late in life. I never had HotWheels toys as a kid. However, after I got my first car I started to learn about all types of cars. Japanese sport cars such as the Subaru WRX STI and the Toyota Supra quickly became my dream cars. Many people state that these cars are the pinnacle of their respective companies and I would have to agree.</p>
 			 </div>
-			 <div class="right-col col-md-4">
+			 <div class="right-col col-md-5">
 				<h2>Cars</h2>
 				<img class="row-image" src="mazda.jpg">
 			</div>
